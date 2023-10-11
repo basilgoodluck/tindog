7509720e8e6fa7dad23e122aa733124e2d38b7f4
basilgoodluck <105502824+basilgoodluck@users.noreply.github.com>
2023-10-11 15:52:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleG = document.querySelector(".toggle-bar")
+const menuBar = document.querySelector("#menu")
+
+toggleG.addEventListener("click",()=>{
+    menuBar.classList.toggle("active")
+    menuBar.focus()
+})
