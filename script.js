89088b2126843cbaa667269a0d95648a0c6d1ae2
Basil Goodluck <105502824+basilgoodluck@users.noreply.github.com>
2023-10-15 22:04:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const toggleG = document.querySelector(".toggle-bar")
-const menuBar = document.querySelector("#menu")
+const toggleG = document.querySelector(".toggle-bar");
+const menuBar = document.querySelector("#menu");
+const lBtn = document.querySelector(".fa-angle-left");
+const rBtn = document.querySelector(".fa-angle-right");
+const slide = document.querySelector(".third-section .content .slide")
+e
 
 toggleG.addEventListener("click",()=>{
     menuBar.classList.toggle("active")
+});
+
+lBtn.addEventListener("click", ()=>{
+    slide.style = `transform: translate(${e})`
 })
